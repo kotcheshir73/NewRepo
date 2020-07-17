3187fe283d22222f256c7927b77f7459807fbf96
kotcheshir73 <kotcheshir73@gmail.com>
2020-07-17 22:27:11 +0400

    добавлен метод

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace ConsoleApp1
 {
 	class Class1
 	{
+		public string GetName() => "Class1";
 	}
 }
